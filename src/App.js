b4ce77d7ec1357d14f66b845fd10869660a609a7
Jared Fipps <jfipps1995@gmail.com>
2023-06-06 15:04:27 -0700

    init commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from "./logo.svg";
+import Hero from "./components/Hero";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <Hero></Hero>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Hero.js
@@ -0,0 +1,70 @@
+import React from "react";
+import Particle from "./Particle";
+import "../css/hero.css";
+
+export default function Hero(props) {
+  return (
+    <section className="HeroSection">
+      <Particle />
+      <div className="HeroText">
+        <h1>This is a test</h1>
+      </div>
+    </section>
+  );
+}
+
+{
+  /* <Particles
+  params={{
+    particles: {
+      number: {
+        value: 160,
+        density: {
+          enable: false,
+        },
+      },
+      size: {
+        value: 3,
+        random: true,
+        anim: {
+          speed: 4,
+          size_min: 0.3,
+        },
+      },
+      line_linked: {
+        enable: false,
+      },
+      move: {
+        random: true,
+        speed: 1,
+        direction: "top",
+        out_mode: "out",
+      },
+    },
+    interactivity: {
+      events: {
+        onhover: {
+          enable: true,
+          mode: "bubble",
+        },
+        onclick: {
+          enable: true,
+          mode: "repulse",
+        },
+      },
+      modes: {
+        bubble: {
+          distance: 250,
+          duration: 2,
+          size: 0,
+          opacity: 0,
+        },
+        repulse: {
+          distance: 400,
+          duration: 4,
+        },
+      },
+    },
+  }}
+/>; */
+}
